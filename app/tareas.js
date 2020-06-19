8c74d138c3993f000ab10923f1b2ce6ace4f1deb
Ezequiel Cortés <codelando@gmail.com>
2020-06-19 20:38:28 -0300

    Bug fix: https://trello.com/c/CNDZDsUF

--- app/tareas.js
+++ app/tareas.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 
 function leerArchivoJSON() {
-    const tareasJson = fs.readFileSync('./tareas.json', 'utf-8');
-    return JSON.parse(tareasJson);
+    if (fs.existsSync('./tareas.json')) {
+        const tareasJson = fs.readFileSync('./tareas.json', 'utf-8');
+        return JSON.parse(tareasJson);
+    }
+
+    return [];
 }
+
 
 function escribirArchivoJSON(tareas) {
     let tareasJson = JSON.stringify(tareas, null, ' ');
